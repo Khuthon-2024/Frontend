0bbd0e0f1e221bcecee5de5076c6cf3f8066b7db
ENvironmentSet <sbshw1@gmail.com>
2024-05-10 21:28:03 +0900

    Add viewbox

---
+++ src/ViewBox.jsx
@@ -0,0 +1,18 @@
+import { css } from '@emotion/react'
+
+export const ViewBox = ({ children }) => (
+  <div
+    css={css`
+      width: 430px;
+      height: 932px;
+      
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      
+      background-color: white;
+    `}
+  >
+    {children}
+  </div>)
