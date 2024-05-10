461ba4cddaa48b5008bda61d8dde2291bd2bb3e3
anfdnfl@naver.com <anfdnfl@khu.ac.kr>
2024-05-11 06:25:49 +0900

    asd

---
+++ src/pages/ImgBox.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+export default function DetailBox({ data }) {
+  const [isDetailOpen, setisDetailOpen] = useState(false);
+
+  const handleOpenDetail = () => setisDetailOpen(true);
+  const handleCloseDetail = () => setisDetailOpen(false);
+
+  const Detail = ({ isOpen, onClose, steps }) => {
+    if (!isOpen) return null;
+
+    return (
+      <div
+        css={css`
+          position: fixed;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background-color: rgba(0, 0, 0, 0.5);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          z-index: 99;
+        `}
+        onClick={handleCloseDetail}
+      >
+        <div
+          css={css`
+            background: white;
+            padding: 20px;
+            border-radius: 20px;
+            width: 75%;
+            height: 35%;
+          `}
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          {steps.map((step, index) => (
+            <div key={index}>{step}</div>
+          ))}
+
+          <div
+            css={css`
+              background-color: #788d64;
+              padding: 15px 30px;
+              margin-bottom: 10px;
+              border-radius: 15vh;
+              transition: transform 0.1s ease;
+              &:active {
+                transform: scale(0.98);
+              }
+              text-align: center;
+            `}
+            onClick={onClose}
+          >
+            피드백 전송 및 다시 요청
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <img src={data} alt="image" onClick={handleOpenDetail} />
+      <Detail isOpen={isDetailOpen} onClose={handleCloseDetail} steps={steps} />
+    </div>
+  );
+}
--- src/pages/RecommendPage.jsx
+++ src/pages/RecommendPage.jsx
@@ -10,70 +10,10 @@ import "./RecommendSlide.css";
 import { EffectCoverflow, Pagination } from "swiper/modules";
 import { css, keyframes } from "@emotion/react";
 import blank from "../assets/blank.png";
+import {ImgBox} from './ImgBox';
 
 // recos: Array<{ image: string, steps: Array<string> }> image is base64 encoded
 export default function RecommendPage({ recos, retry }) {
-  const [isDetailOpen, setisDetailOpen] = useState(false);
-
-  const handleOpenDetail = () => setisDetailOpen(true);
-  const handleCloseDetail = () => setisDetailOpen(false);
-
-  const Detail = ({ isOpen, onClose, steps }) => {
-    if (!isOpen) return null;
-
-    return (
-      <div
-        css={css`
-          position: fixed;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          background-color: rgba(0, 0, 0, 0.5);
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          z-index: 99;
-        `}
-        onClick={handleCloseDetail}
-      >
-        <div
-          css={css`
-            background: white;
-            padding: 20px;
-            border-radius: 20px;
-            width: 75%;
-            height: 35%;
-          `}
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
-        >
-          {steps.map((step,index) => (
-            <div key={index}>{step}</div>
-          ))}
-
-          <div
-            css={css`
-              background-color: #788d64;
-              padding: 15px 30px;
-              margin-bottom: 10px;
-              border-radius: 15vh;
-              transition: transform 0.1s ease;
-              &:active {
-                transform: scale(0.98);
-              }
-              text-align: center;
-            `}
-            onClick={onClose}
-          >
-            피드백 전송 및 다시 요청
-          </div>
-        </div>
-      </div>
-    );
-  };
-
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleOpenModal = () => setIsModalOpen(true);
@@ -209,8 +149,7 @@ export default function RecommendPage({ recos, retry }) {
       >
         {recos.map(({ image, steps }, index) => (
           <SwiperSlide key={index}>
-            <img src={`data:image/png;base64,${image}`} alt="img" onClick={handleOpenDetail}/>
-            <Detail isOpen={isDetailOpen} onClose={handleCloseDetail} steps={steps} />
+            <ImgBox data={`data:image/png;base64,${image}`}/>
           </SwiperSlide>
         ))}
       </Swiper>
